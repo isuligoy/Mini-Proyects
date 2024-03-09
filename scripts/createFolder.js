bd3695ba8e4af351e4007175bbd7bb8a4eab73b4
isuligoy <ignaciosuligoy@gmail.com>
2024-03-09 04:33:33 -0300

    Add Script for create folder

---
+++ scripts/createFolder.js
@@ -0,0 +1,73 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+// npm run cf <name-folder>
+// console.dir(process.argv[2]);
+// console.dir(process.argv.slice(2));
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const publicPath = path.join(__dirname, "../public/projects/");
+const argFolderName = process.argv[2];
+
+const contentHTML = (folderName) => `
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${folderName}</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Hola, mundo!</h1>
+  <script src="script.js"></script>
+</body>
+</html>
+`;
+
+const contentCSS = `
+body {
+  background-color: #f0f0f0;
+}
+`;
+
+const contentJS = `
+console.log('Hola, mundo!');
+`;
+
+//Create the folder
+async function createNewFolder(folderName) {
+    if (!folderName) {
+        return console.error("Error: Debes proporcionar un nombre de carpeta.");
+    }
+
+    const lastFolderNumber = await getLastFolderNumber();
+    const folder = path.join(publicPath, lastFolderNumber + "-" + folderName);
+    try {
+        await fs.mkdir(folder);
+        await Promise.all([
+            fs.writeFile(
+                path.join(folder, "index.html"),
+                contentHTML(folderName)
+            ),
+            fs.writeFile(path.join(folder, "styles.css"), contentCSS),
+            fs.writeFile(path.join(folder, "script.js"), contentJS),
+        ]);
+    } catch (error) {
+        console.error("Error", error);
+    }
+}
+
+// Get de last folder in number
+async function getLastFolderNumber() {
+    const folders = await fs.readdir(publicPath);
+    const numbers = folders.map((folder) => parseInt(folder));
+    const result = numbers.length ? Math.max(...numbers) + 1 : 0;
+    return String(result).padStart(2, "0");
+}
+
+// Added to the json
+
+// Create new folder
+createNewFolder(argFolderName);
