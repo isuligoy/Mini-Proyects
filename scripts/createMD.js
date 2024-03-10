9b10fbbedac61e252488e617e83bd701e4d588f6
isuligoy <87563299+isuligoy@users.noreply.github.com>
2024-03-10 13:48:51 -0300

    🚀🚀🚀

--- scripts/createMD.js
+++ scripts/createMD.js
@@ -1,11 +1,32 @@
+import proyects from "@scripts/projects.json";
+
 const PLACEHOLDER = {
+    GITHUB_URL: "https://isuligoy.github.io/Mini-Proyects/",
     PROYECT_NUMBER: "%{{number}}%",
     LINK_PAGE: "%{{link_page}}%",
     LINK_CODE: "%{{link_code}}%",
 };
 
+const tableTemplete = ({ PROYECT_NUMBER, LINK_PAGE, LINK_CODE }) => `<table>
+<tr>
+    <th>Number</th>
+    <th>Page</th>
+    <th>Code</th>
+</tr>
+<tr>
+    <td>${PROYECT_NUMBER}</td>
+    <td>${LINK_PAGE}</td>
+    <td>${LINK_CODE}</td>
+</tr>
+</table>`;
+
 const getProyectNumber = () => {};
 const getProyectPageLink = () => {};
 const getProyectCodeLink = () => {};
 
-const generateTable = () => {};
+const generateTable = () => {
+    proyects.map((proyect) => {
+        const { name, number, url } = proyect;
+    });
+};
+generateTable();
