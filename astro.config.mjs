aa88a99ef00e1c7bde0171b7750282de2a4c60d3
isuligoy <87563299+isuligoy@users.noreply.github.com>
2024-03-09 21:12:50 -0300

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 // https://astro.build/config
+import path from "node:path";
+
+const isGitHubPages = true;
+const folderName = path.basename(process.cwd()) + "/";
+const mode =
+    process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
 export default defineConfig({
     root: "./",
+    base,
+    mode,
     envDir: "./",
     publicDir: "./public",
     build: {
