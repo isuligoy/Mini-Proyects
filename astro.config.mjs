f9772bd7bea13c28182e453f9a308f9c890919cb
isuligoy <87563299+isuligoy@users.noreply.github.com>
2024-03-09 21:46:24 -0300

    Set a new file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import path from "node:path";
+// import path from "node:path";
 
-const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
-const mode =
-    process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+// const isGitHubPages = true;
+// const folderName = path.basename(process.cwd()) + "/";
+// const mode =
+//     process.env.NODE_ENV === "production" ? "production" : "development";
+// const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
 export default defineConfig({
-    site: "https://atomcowork.github.io",
+    // site: "https://isuligoy.github.io/Mini-Proyects/",
     root: "./",
-    base,
-    mode,
+    // base,
+    // mode,
     envDir: "./",
     publicDir: "./public",
-    resolve: {
-        alias: {
-            "@": new URL("./src", import.meta.url).pathname,
-        },
-    },
+    // resolve: {
+    //     alias: {
+    //         "@": new URL("./src", import.meta.url).pathname,
+    //     },
+    // },
     build: {
         outDir: "./dist",
         assetsDir: "./",
