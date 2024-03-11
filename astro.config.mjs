4874eda4ac215b8f79b11460828a5f0ace9576d7
isuligoy <ignaciosuligoy@gmail.com>
2024-03-11 02:29:24 -0300

    Solve and remove unnecessary staff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,29 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import path from "node:path";
+// import path from "node:path";
 
-const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
-const mode =
-    process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+// const isGitHubPages = true;
+// const folderName = path.basename(process.cwd()) + "/";
+// const mode =
+//     process.env.NODE_ENV === "production" ? "production" : "development";
+// const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
 export default defineConfig({
     site: "https://isuligoy.github.io",
-    root: "./",
-    base,
-    mode,
-    envDir: "./",
-    publicDir: "./public",
-    resolve: {
-        alias: {
-            "@": new URL("./src", import.meta.url).pathname,
-        },
-    },
-    build: {
-        outDir: "./dist",
-        assetsDir: "./",
-    },
+    base: "/",
+    // root: "./",
+    // base,
+    // mode,
+    // envDir: "./",
+    // publicDir: "./public",
+    // resolve: {
+    //     alias: {
+    //         "@": new URL("./src", import.meta.url).pathname,
+    //     },
+    // },
+    // build: {
+    //     outDir: "./dist",
+    //     assetsDir: "./",
+    // },
 });
