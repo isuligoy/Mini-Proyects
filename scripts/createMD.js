89e1432c348334b1c9e45442f57302898063e05d
isuligoy <ignaciosuligoy@gmail.com>
2024-03-20 20:19:36 -0300

    feat: new badges for page and code

--- scripts/createMD.js
+++ scripts/createMD.js
@@ -1,26 +1,28 @@
 import fs from "node:fs/promises";
-/*
-const getProyectNumber = () => {};
-*/
-const getProyectUrlLink = (url) => {
-    return `[Link](${PLACEHOLDER.GITHUB_URL + url})`;
+const getProyectCodeLink = (url) => {
+    return BADGES.LINK_CODE(PLACEHOLDER.GITHUB_URL + url);
 };
-const getProyectNameLink = (name) => {
-    return `[Link](${PLACEHOLDER.GITHUB_PAGE + name})`;
+const getProyectPageLink = (name) => {
+    return BADGES.LINK_PAGE(PLACEHOLDER.GITHUB_URL + name);
 };
 
 const PLACEHOLDER = {
     GITHUB_PAGE:
         "https://github.com/isuligoy/Mini-Proyects/tree/main/public/projects/",
     GITHUB_URL: "https://isuligoy.github.io/Mini-Proyects/",
-    TEMPLATE: "%{{table_templete}}%",
-    /* PROYECT_NUMBER: "%{{number}}%",
-    LINK_PAGE: "%{{link_page}}%",
-    LINK_CODE: "%{{link_code}}%",*/
+    TEMPLATE_TABLE: "%{{table_templete}}%",
+    TEMPLATE_DATE: "%{{formated_date}}%",
+};
+
+const BADGES = {
+    LINK_PAGE: (URL) =>
+        `[![GitHub Badge](https://img.shields.io/badge/Código-181717?logo=react&logoColor=fff&style=flat-square)](${URL})`,
+    LINK_CODE: (URL) =>
+        `[![Link Badge](https://img.shields.io/badge/Page-181717?logo=github&logoColor=fff&style=flat-square)](${URL})`,
 };
 
 const tableTemplete = (proyects) => {
-    const table = `| Number | Proyecto Name | Page | Code |\n| :----: |  :----: |  :----: | :----: |\n${generateTable(
+    const table = `| Number | Proyecto Name | Page | Code |\n| :----: |  :---- |  :----: | :----: |\n${generateTable(
         proyects
     )}`;
     return table;
@@ -29,9 +31,9 @@ const tableTemplete = (proyects) => {
 const generateTable = (proyects) => {
     const tableDataRows = proyects
         .map(({ name, number, url }) => {
-            return `| # ${number} | ${name} | ${getProyectUrlLink(
+            return `| # ${number} | ${name} | ${getProyectCodeLink(
                 url
-            )}| ${getProyectNameLink(name)} |`;
+            )}| ${getProyectPageLink(name)} |`;
         })
         .join("\n");
     return tableDataRows;
@@ -57,7 +59,7 @@ async function writeReadMe(tableWriten) {
             }
         );
         const newMarkDown = getTemplateMarkdown.replace(
-            PLACEHOLDER.TEMPLATE,
+            PLACEHOLDER.TEMPLATE_TABLE,
             tableWriten
         );
         await fs.writeFile("ReadMe.md", newMarkDown);
