d1f07b4da62a67a4cf4553cd67bcb07b7e7d39d9
isuligoy <87563299+isuligoy@users.noreply.github.com>
2024-03-28 13:15:56 -0300

    Add create last gh action

--- scripts/getDateForAction.js
+++ scripts/getDateForAction.js
@@ -1,13 +1,23 @@
 const link =
     "https://api.github.com/repos/isuligoy/Mini-Proyects/actions/workflows/deploy.yml/runs";
 
-async function getDate() {
+export async function getDate() {
     try {
         const data = await fetch(link);
         const info = await data.json();
-        console.log(info.workflow_runs[0].created_at);
+        const date = info.workflow_runs[0].created_at;
+        return formatedDate({ date });
     } catch (err) {
         console.log(err);
     }
 }
-getDate();
+
+export default function formatedDate({ date }) {
+    const newDate = new Date(date);
+    const dia = newDate.getDate().toString().padStart(2, "0");
+    const mes = (newDate.getMonth() + 1).toString().padStart(2, "0");
+    const año = newDate.getFullYear();
+
+    const fechaFormateada = `${dia}/${mes}/${año}`;
+    return fechaFormateada;
+}
