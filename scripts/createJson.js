4908e9ef845fbcc6ddca03617802f058fcef31e0
isuligoy <ignaciosuligoy@gmail.com>
2024-04-10 08:44:57 -0300

    Add test for deploy

--- scripts/createJson.js
+++ scripts/createJson.js
@@ -1,7 +1,6 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import crypto from "node:crypto";
 
 const PROYECTS_PATH = {
     REAL_FOLDER: "projects/",
